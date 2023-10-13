5c810967c0dca221b677a473e67dc208c600bcdc
qzip <altandf4alt@gmail.com>
2023-10-13 21:57:53 +0300

    modified:   .gitignore 	new file:   bin/.gitkeep 	new file:   cache/.gitkeep 	new file:   controllers/.gitkeep 	new file:   files/.gitkeep 	new file:   plugins/.gitkeep 	new file:   templates/.gitkeep 	new file:   translations/.gitkeep 	new file:   vendor/autoload.php

---
+++ vendor/composer/installed.php
@@ -0,0 +1,51 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '3d28bc599e9199593f7979989b2859107c558066',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '3d28bc599e9199593f7979989b2859107c558066',
+    ),
+    'symfony/deprecation-contracts' => 
+    array (
+      'pretty_version' => 'v3.3.0',
+      'version' => '3.3.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '7c3aff79d10325257a001fcf92d991f24fc967cf',
+    ),
+    'symfony/polyfill-ctype' => 
+    array (
+      'pretty_version' => 'v1.27.0',
+      'version' => '1.27.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5bbc823adecdae860bb64756d639ecfec17b050a',
+    ),
+    'symfony/yaml' => 
+    array (
+      'pretty_version' => 'v6.3.3',
+      'version' => '6.3.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'e23292e8c07c85b971b44c1c4b87af52133e2add',
+    ),
+  ),
+);
